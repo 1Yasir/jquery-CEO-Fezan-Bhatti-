9ffc5428f772ef86cd36847e25d60935a331d327
Yasir Mehmood <129255451+1Yasir@users.noreply.github.com>
2024-08-28 01:26:38 +0500

    we lerrned that how select the child select and hide the elemnt when clicked - add unorder list and disply the UI

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-// select an button with id #alertMessage
+$(document).ready(function () {
+    // Select the button with id #alertMessage and show an alert when clicked
+    $("#alertMessage").click(function () {
+        alert("Hello JQUERY.....");
+    });
 
-$("#alertMessage").click(()=>{
-    alert("hello world!")
-})
+    // Select list items inside the unordered list and hide the clicked item
+    $("#unOrderList ul li").click(function () {
+        $(this).hide();
+    });
+});
